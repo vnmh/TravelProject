e805993bcbd93a40f05460e7ea510db8a20255e7
Minh Hiền <minhhien@some-email>
2021-05-01 10:16:35 +0700

    save code

--- be/routes/auth.js
+++ be/routes/auth.js
@@ -7,74 +7,40 @@ const authController = require('../controllers/auth');
 
 const router = express();
 
-const isAuth = require('../middleware/is-auth')
+const isAuth = require('../middleware/is-auth');
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + file.originalname);
-  }
+   destination: (req, file, cb) => {
+      cb(null, './images');
+   },
+   filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + file.originalname);
+   }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
 };
 
-const upload = multer(
-  { storage: fileStorage, 
-    limits: {
-      fileSize: 1024*1024*5
-    }, 
-    fileFilter: fileFilter 
-  })
+const upload = multer({
+   storage: fileStorage,
+   limits: {
+      fileSize: 1024 * 1024 * 5
+   },
+   fileFilter: fileFilter
+});
 // const upload = multer({dest: 'images/'})
-router.post(
-  '/ThemNguoiDung', upload.single('avatar'),
-  [
-   
-  ],
-  authController.signup,
-  
-)
-router.post(
-  '/ThemAdmin', upload.single('avatar'),
-  [
-   
-  ],
-  authController.insertuser,
-  
-)
-router.post('/DangNhap',upload.single('avatar'),
-  [
-  ], 
-  authController.login,
-);
+router.post('/ThemNguoiDung', upload.single('avatar'), [], authController.signup);
+router.post('/ThemAdmin', upload.single('avatar'), [], authController.insertuser);
+router.post('/DangNhap', upload.single('avatar'), [], authController.login);
 
-router.get('/LayDanhSachLoaiNguoiDung',
-  [
-  ], 
-  authController.getListUser,
-);
+router.get('/LayDanhSachLoaiNguoiDung', [], authController.getListUser);
 
-router.post('/CapNhatThongTin',upload.single('avatar'),
-  [
-  ], 
-  authController.updateProfile,
-);
-router.delete(
-  '/XoaNguoiDung/:TaiKhoan',
-  isAuth,
-  authController.deleteUser
-)
+router.post('/CapNhatThongTin', upload.single('avatar'), [], authController.updateProfile);
+router.delete('/XoaNguoiDung/:TaiKhoan', isAuth, authController.deleteUser);
 
 module.exports = router;
